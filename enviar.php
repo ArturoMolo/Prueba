abed96032eeb619d10c1a600c514c8b514998733
ArturoMolo <64377550+ArturoMolo@users.noreply.github.com>
2020-06-20 16:08:27 +0200

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,22 +1,45 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
+/**
+ * Simple formulario de contacto con PHP
+ *
+ * @author parzibyte
+ * @see https://parzibyte.me/blog
+ */
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+if (empty($_POST["nombre"])) {
+    exit("Falta el nombre");
+}
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+if (empty($_POST["correo"])) {
+    exit("Falta el correo");
+}
 
-$para = 'arthur_n09@hotmail.com';
-$asunto = 'Mensaje de mi sitio web';
+if (empty($_POST["mensaje"])) {
+    exit("Falta el mensaje");
+}
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$mensaje = $_POST["mensaje"];
 
-header("Location:index.html");
-?>
+$correo = filter_var($correo, FILTER_VALIDATE_EMAIL);
+if (!$correo) {
+    echo "Correo inválido. Intenta con otro correo.";
+    exit;
+}
+
+$asunto = "Nuevo mensaje de sitio web";
+
+$datos = "De: $nombre\nCorreo: $correo\nMensaje: $mensaje";
+$mensaje = "Has recibido un mensaje desde el formulario de contacto de tu sitio web. Aquí están los detalles:\n$datos";
+$destinatario = "arthur_n09@hotmail.com"; # aquí la persona que recibirá los mensajes
+$encabezados = "Sender: correo@dominio.com\r\n"; # El remitente, debe ser un correo de tu dominio de servidor
+$encabezados .= "From: $nombre <" . $correo . ">\r\n";
+$encabezados .= "Reply-To: $nombre <$correo>\r\n";
+$resultado = mail($destinatario, $asunto, $mensaje, $encabezados);
+if ($resultado) {
+    echo "<h1>Mensaje enviado, ¡Gracias por contactarme!</h1>";
+    echo "<p>Tu mensaje se ha enviado correctamente.</p><h2>Importante</h2><p>Revisa tu bandeja de SPAM, en ocasiones mis respuestas quedan ahí. </p>";
+} else {
+    echo "Tu mensaje no se ha enviado. Intenta de nuevo.";
+}
